7f5babbe5ef69d3beb1eeb66c91a37ed190ad6b9
dkabe <devika.kabe@gmail.com>
2021-07-04 20:22:47 -0400

    model updates

--- run_instances_all.py
+++ run_instances_all.py
@@ -1,17 +1,17 @@
-from stochastic_resilience import *
+from stochastic_resilience_v3 import *
 import time
-instances = 5
-MPs = [2, 3, 4, 6, 6, 6]
-DCs = [3, 4, 6, 8, 4, 4]
-MZs = [1, 2, 3, 5, 29, 29]
-numScenarios = [32, 128, 200, 200, 200, 200]
-epsilons = [200, 400, 600, 800, 1000000, 700000]
-rl = [0.5 , 0.55, 0.6 , 0.65, 0.7, 0.75, 0.8 , 0.85, 0.9 , 0.95]
+instances = 4
+MPs = [2, 3, 4, 6]
+DCs = [3, 4, 6, 8]
+MZs = [1, 2, 3, 5]
+numScenarios = [32, 128, 200, 200]
+epsilons = [200, 400, 600, 800]
+rl = [0.9, 0.8, 0.7]
 
-for instance in range(5,6):
+for instance in range(instances):
     for r_level in rl:
-            start_time = time.time()
-            run_Model(instance, r_level, numScenarios[instance], MPs[instance], DCs[instance], MZs[instance], Products[instance], Outsourced[instance], epsilons[instance], {'f1': 1, 'f2': 1})
-            end_time = time.time()
-            print('CPU: ', end_time - start_time)
-            print('\n')
+        start_time = time.time()
+        run_Model(instance, r_level, numScenarios[instance], MPs[instance], DCs[instance], MZs[instance], Products, Outsourced, epsilons[instance], {'f1': 0.7, 'f2': 0.3})
+        end_time = time.time()
+        print('CPU: ', end_time - start_time)
+        print('\n')
