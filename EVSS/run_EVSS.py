14afb9f2fba5a0336c4f1c843f2908aab3d37a6a
Devika Kabe <devika.kabe@gmail.com>
2021-07-07 15:43:11 -0400

    EVSS

--- EVSS/run_EVSS.py
+++ EVSS/run_EVSS.py
@@ -6,9 +6,10 @@ DCs = [3, 4, 6, 8, 4, 4]
 MZs = [1, 2, 3, 5, 29, 29]
 numScenarios = [32, 128, 200, 200, 128, 200]
 epsilons = [200, 400, 600, 800, 1500000, 700000]
-rl = [0.95]
+rl = [0.5]
 
 for instance in range(4,5):    
     for r_level in rl:
         for scen in range(numScenarios[instance]):
+            grbModel = Model('EVSS')
             run_Model(instance, r_level, numScenarios[instance], MPs[instance], DCs[instance], MZs[instance], Products[instance], Outsourced[instance], epsilons[instance], scen)
