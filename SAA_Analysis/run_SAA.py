9ed818b2c6e765c90da8bfbd7f85e3584a8496f2
Devika Kabe <devika.kabe@gmail.com>
2021-07-27 13:42:29 -0400

    SAA

---
+++ SAA_Analysis/run_SAA.py
@@ -0,0 +1,12 @@
+from SAA import *
+import time
+instance = 5
+MPs = [2, 3, 4, 6, 6, 6]
+DCs = [3, 4, 6, 8, 4, 4]
+MZs = [1, 2, 3, 5, 29, 29]
+epsilons = [200, 400, 600, 800, 1500000, 700000]
+rl = 0.5
+batches = 30
+num_Scenarios = 150
+for batch in range(20,batches):
+    run_Model(instance, rl, num_Scenarios, MPs[instance], DCs[instance], MZs[instance], Products[instance], Outsourced[instance], epsilons[instance], batch)
