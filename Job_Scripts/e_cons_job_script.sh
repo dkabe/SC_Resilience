2ec8c10b668fa157e2800932b5928c6155d36a9f
Devika Kabe <devika.kabe@gmail.com>
2021-08-10 09:41:15 -0400

    epsilon constraint

---
+++ Job_Scripts/e_cons_job_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --account=def-wahab
+#SBATCH --time=0-02:00        # time limit (D-HH:MM)
+#SBATCH --cpus-per-task=40     # number of CPUs (threads) to use
+#SBATCH --mem-per-cpu=1000M   # memory per CPU (in MB)
+module load nixpkgs/16.09
+module load gurobi/9.0.1
+source ~/env_gurobi/bin/activate
+python /home/dkabe/Model_brainstorming/Epsilon_Constraint/Three_Objectives/run_epsilon_constraint.py
