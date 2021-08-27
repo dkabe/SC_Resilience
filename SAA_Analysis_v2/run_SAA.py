64cc84e5c6d29bdeff0e224d752b2183eaa3f0e2
Devika Kabe <devika.kabe@gmail.com>
2021-08-27 09:24:35 -0400

    git reorg

--- SAA_Analysis_v2/run_SAA.py
+++ SAA_Analysis_v2/run_SAA.py
@@ -6,7 +6,8 @@ DCs = [4, 4]
 MZs = [29, 29]
 epsilons = [1500000, 700000]
 rl = 0.5
-batches = range(30)
+batches = [5, 12, 25]
+
 #N = [100, 200, 300, 400, 500]
 N = [1024]
 
@@ -15,5 +16,5 @@ N = [1024]
  #   run_Model(instance, rl, num_Scenarios, MPs[instance], DCs[instance], MZs[instance], Products[instance], Outsourced[instance], epsilons[instance], batch)
 
 for num_scenarios in N:
-    with mp.Pool(30) as pool:
-        pool.starmap(run_Model, [(num_scenarios, batch) for batch in (batches)])
+    with mp.Pool(3) as pool:
+        pool.starmap(run_Model, [(num_scenarios, batch) for batch in batches])
