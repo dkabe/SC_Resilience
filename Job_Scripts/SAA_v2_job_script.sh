dff1e71cc84a83a99b1e05bde834048f7ce6e055
Devika Kabe <devika.kabe@gmail.com>
2021-08-27 12:52:27 -0400

    saa

--- Job_Scripts/SAA_v2_job_script.sh
+++ Job_Scripts/SAA_v2_job_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=def-wahab
-#SBATCH --time=0-020:00        # time limit (D-HH:MM)
+#SBATCH --time=0-04:00        # time limit (D-HH:MM)
 #SBATCH --cpus-per-task=30     # number of CPUs (threads) to use
-#SBATCH --mem-per-cpu=25000M   # memory per CPU (in MB)
+#SBATCH --mem-per-cpu=2000M   # memory per CPU (in MB)
 module load nixpkgs/16.09
 module load gurobi/9.0.1
 source ~/env_gurobi/bin/activate
-python /home/dkabe/Model_brainstorming/SAA_Analysis_v3/run_SAA.py
+python /home/dkabe/Model_brainstorming/SAA_Analysis_v3/Instance_1/run_SAA.py
---
+++ SAA_Analysis_v3/Instance_1/run_SAA.py
@@ -0,0 +1,19 @@
+from SAA import *
+import multiprocessing as mp 
+instance = 0
+MPs = [6, 6]
+DCs = [4, 4]
+MZs = [29, 29]
+epsilons = [1500000, 700000]
+rl = 0.5
+batches = range(30)
+N = [32, 64, 96, 128, 160, 192]
+
+
+#for batch in range(batches):
+
+ #   run_Model(instance, rl, num_Scenarios, MPs[instance], DCs[instance], MZs[instance], Products[instance], Outsourced[instance], epsilons[instance], batch)
+
+for num_scenarios in N:
+    with mp.Pool(30) as pool:
+        pool.starmap(run_Model, [(num_scenarios, batch) for batch in batches], chunksize=1)
