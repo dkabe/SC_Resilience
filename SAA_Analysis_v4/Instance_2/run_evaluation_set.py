fbba595fcb122ded5215b7fa908ed8125f3018fd
Devika Kabe <devika.kabe@gmail.com>
2021-08-31 12:31:00 -0400

    updates

--- SAA_Analysis_v4/Instance_2/run_evaluation_set.py
+++ SAA_Analysis_v4/Instance_2/run_evaluation_set.py
@@ -8,13 +8,13 @@ DCs = [4, 4]
 MZs = [29, 29]
 epsilons = [1500000, 700000]
 rl = 0.5
-batches = 30
+batches = range(30)
 N = [64, 128, 192, 256, 320, 384, 448]
 N = [448]
 start_time = time.time()
 
 for num_Scenarios in N:
-    for scen in range(9503,10000):
+    for scen in range(10000):
         with mp.Pool(30) as pool:
-            pool.starmap(run_Model, [(num_Scenarios, scen, batch) for batch in range(batches)], chunksize=1)
+            pool.starmap(run_Model, [(num_Scenarios, scen, batch) for batch in (batches)], chunksize=1)
         pool.close()
