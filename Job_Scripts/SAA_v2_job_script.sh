9f5d826477f3b9f8eb1ea7356d8f7f0b751918a2
Devika Kabe <devika.kabe@gmail.com>
2021-08-30 21:13:52 -0400

    SAA

--- Job_Scripts/SAA_v2_job_script.sh
+++ Job_Scripts/SAA_v2_job_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=def-wahab
-#SBATCH --time=0-02:00        # time limit (D-HH:MM)
+#SBATCH --time=0-03:00        # time limit (D-HH:MM)
 #SBATCH --cpus-per-task=30     # number of CPUs (threads) to use
 #SBATCH --mem-per-cpu=4000M   # memory per CPU (in MB)
 module load nixpkgs/16.09
 module load gurobi/9.0.1
 source ~/env_gurobi/bin/activate
-python /home/dkabe/Model_brainstorming/SAA_Analysis_v4/Instance_1/run_SAA.py
+python /home/dkabe/Model_brainstorming/SAA_Analysis_v4/Instance_2/run_SAA.py
